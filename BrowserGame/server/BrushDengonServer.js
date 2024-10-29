bf3510c8cfb158556cdeb1393e53d521ed0798a7
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-10-29 22:06:25 +0900

    brushdengon client

--- BrowserGame/server/BrushDengonServer.js
+++ BrowserGame/server/BrushDengonServer.js
@@ -4,3 +4,23 @@ const http = require('http');
 const server= http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../html/brush_dengon_draw.html');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+server.listen(7000,()=>{
+    console.log("BrushDengon Listening on port : 7000");
+});
