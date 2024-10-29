3205e0f6be54501fdbf69f986a1aceddee929c7d
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-10-29 22:17:53 +0900

    client page path change on server

--- BrowserGame/server/BrushDengonServer.js
+++ BrowserGame/server/BrushDengonServer.js
@@ -5,8 +5,8 @@ const server= http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '../html/brush_dengon_draw.html');
+app.get('../', (req, res) => {
+    res.sendFile(__dirname + '/html/brush_dengon_draw.html');
 });
 
 
