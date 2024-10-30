a9a14200b187657a4040ebde16d538e1ecfc22e4
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-10-30 18:55:24 +0900

    test

--- BrowserGame/server/BrushDengonServer.js
+++ BrowserGame/server/BrushDengonServer.js
@@ -5,7 +5,7 @@ const server= http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/brush_dengon_draw.html');
