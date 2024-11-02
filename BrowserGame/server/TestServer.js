21a2396619b54d912c62639b0b685117bbc39aef
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-11-02 19:26:02 +0900

    connection test

--- BrowserGame/server/TestServer.js
+++ BrowserGame/server/TestServer.js
@@ -1,4 +1,9 @@
-const options = { /* ... */ };
+const options = {
+    cors: {
+        origin: "http://52.68.111.88:7000",
+        methods: ["GET", "POST"]
+    }
+};
 const io = require("socket.io")(7000, options);
 
 io.on("connection", socket => {
