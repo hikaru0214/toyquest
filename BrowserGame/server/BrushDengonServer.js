89b8194c047c6433b82821c2d7f6ed304f8b994f
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-11-02 19:20:13 +0900

    connection test

--- BrowserGame/server/BrushDengonServer.js
+++ BrowserGame/server/BrushDengonServer.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server= http.createServer(app);
-const { Server } = require("socket.io")(server, {
-    cors: {
-      origin: '*',
-    }
-  });
+const { Server } = require("socket.io");
 const io = new Server(server);
 const path = require('path');
 
---
+++ BrowserGame/server/TestServer.js
@@ -0,0 +1,9 @@
+const options = { /* ... */ };
+const io = require("socket.io")(7000, options);
+
+io.on("connection", socket => {
+    console.log('a user connected');
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    });
+});
