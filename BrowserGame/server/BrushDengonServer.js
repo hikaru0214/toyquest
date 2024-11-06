55950f91bd7a3c65d032b69d96b705cbf1931566
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-11-07 03:13:13 +0900

    draw relay fix

--- BrowserGame/server/BrushDengonServer.js
+++ BrowserGame/server/BrushDengonServer.js
@@ -89,7 +89,7 @@ io.on('connection', (socket) => {
 
     socket.on("client draw",(data)=>{
         if(gamerooms[room].getGameState()=="standby"||gamerooms[room].isDrawing(id)){
-            socket.broadcast.to(room_name).emit("draw relay",data);
+            io.broadcast.to(room_name).emit("draw relay",data);
             console.log("relaying drawing to clients!");
         }
     });
