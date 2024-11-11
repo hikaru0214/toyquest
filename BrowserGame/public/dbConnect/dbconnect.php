10a7973da580e0d9eec2d7815881942880f334ea
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2024-11-11 12:48:38 +0900

    testDB

--- BrowserGame/public/dbConnect/dbconnect.php
+++ BrowserGame/public/dbConnect/dbconnect.php
@@ -9,11 +9,6 @@ $connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
 try {
     $pdo = new PDO($connect, USER, PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = $pdo->query("select * from User");
-    $select = $query->fetchAll();
-    foreach ($select as $row) {
-        echo $row['id'];
-    }
 } catch (PDOException $e) {
     die('データベース接続失敗: ' . $e->getMessage());
 }
