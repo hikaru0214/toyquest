8db9fabeb7a492c87e41e270aeefafd8d4188c0e
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2024-11-11 12:33:52 +0900

    dbconnect.php変更

--- BrowserGame/public/dbConnect/dbconnect.php
+++ BrowserGame/public/dbConnect/dbconnect.php
@@ -9,9 +9,6 @@ $connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
 try {
     $pdo = new PDO($connect, USER, PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = $pdo->query("select * from User");
-    $select = $query->fetchAll();
-    var_dump($select);
 } catch (PDOException $e) {
     die('データベース接続失敗: ' . $e->getMessage());
 }
