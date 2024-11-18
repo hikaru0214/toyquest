468ea8215199e28a1b4d75171813c6af0e31a418
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-11-18 09:43:25 +0900

    login-process-modify

--- BrowserGame/public/php/login.php
+++ BrowserGame/public/php/login.php
@@ -23,7 +23,7 @@
 
 <div style="width: 50vw; height: 50vh; margin-left: auto; margin-right: auto; text-align: center; font-size: large; position: relative;">
 
-    <form action="/login-process.php" method="post">
+    <form action="/BrowserGame/public/php/login-process.php" method="post">
         メールアドレス <input id="email" name="email" type="text" style="margin-top: 50vh;">
         <br>
         パスワード <input id="password" name="password" type="text">
