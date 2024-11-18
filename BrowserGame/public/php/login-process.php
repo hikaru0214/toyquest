266f06fa6a8a94a80df4ffbea389dfe6aa262ba7
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-11-18 10:56:48 +0900

    login-process-modify

--- BrowserGame/public/php/login-process.php
+++ BrowserGame/public/php/login-process.php
@@ -1,6 +1,9 @@
 <?php session_start(); ?>
 <?php require '../dbConnect/db-connect.php';?>
-<?php 
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // $sql=$pdo->prepare('select * from User where mailaddress = ? AND password = ?');
 // $sql->execute([$_POST['email'],$_POST['password']]);
 // foreach($sql as $row) {
@@ -12,8 +15,12 @@
 //     ];
 // }
 // if(isset($_SESSION['user'])){
+try{
 header('Location: BrowserGame/public/php/top.php');
 exit;
+}catch(Exception e){
+    var_dump($e->getMessage());
+}
 // }else{
 // header('Location: BrowserGame/public/php/login.php?hogeA=ログイン名またはパスワードが違います');
 // exit;
