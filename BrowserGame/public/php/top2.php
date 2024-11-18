c7ba2d36bb709a6e240fdcad2c63dbce55600863
2201150 <147681633+2201150@users.noreply.github.com>
2024-11-18 12:35:33 +0900

    top

--- BrowserGame/public/php/top2.php
+++ BrowserGame/public/php/top2.php
@@ -441,15 +441,15 @@
             <td>zzz</td>
         </tr>
         <?php
-$sql=$pdo->prepare('SELECT user_id,SUM(score) AS total_score FROM Score GROUP BY user_id ORDER BY total_score ASC');
-$sql->execute();
-foreach($sql as $row) {
-        echo "</td><td>"+$row['user_name'];
-        echo "</td><td>"+$row['total_score'];
-        echo "</td></tr>";
-}
-echo "<tr><td>終了<td></tr>";
-?>
+        $sql=$pdo->prepare('SELECT user_id,user_name_SUM(score) AS total_score FROM Score GROUP BY user_id ORDER BY total_score ASC');
+        $sql->execute();
+        foreach($sql as $row) {
+                echo "</td><td>".$row['user_name'];
+                echo "</td><td>".$row['total_score'];
+                echo "</td></tr>";
+        }
+        echo "<tr><td>終了<td></tr>";
+        ?>
     </table>
     testtesttest
 </div>
