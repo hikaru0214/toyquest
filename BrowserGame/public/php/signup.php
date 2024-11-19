9ad0c43cacfcd14a42fca995527b353b0edd894a
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-11-19 10:08:52 +0900

    add

--- BrowserGame/public/php/signup.php
+++ BrowserGame/public/php/signup.php
@@ -1,7 +1,7 @@
-<?php session_start(); 
+<?php session_start();?>
 <!-- DB接続 -->
- require '../dbConnect/dbconnect.php'; 
-
+ <?require '../dbConnect/dbconnect.php';?>
+<?
 $db['host'] = "localhost";  // DBサーバのURL
 $db['user'] = "hogeUser";  // ユーザー名
 $db['pass'] = "hogehoge";  // ユーザー名のパスワード
