05bc38ff57ed0c0f28acf92bbcfbe9735ff44c3a
2201150 <147681633+2201150@users.noreply.github.com>
2024-11-19 09:30:13 +0900

    top

--- BrowserGame/public/php/top2.php
+++ BrowserGame/public/php/top2.php
@@ -441,7 +441,7 @@
             <td>zzz</td>
         </tr>
         <?php
-        $sql=$pdo->prepare('SELECT user_id,user_name_SUM(score) AS total_score FROM Score GROUP BY user_id ORDER BY total_score ASC');
+        $sql=$pdo->prepare('SELECT user_id,user_name SUM(score) AS total_score FROM Score GROUP BY user_id ORDER BY total_score ASC');
         $sql->execute();
         foreach($sql as $row) {
                 echo "</td><td>".$row['user_name'];
