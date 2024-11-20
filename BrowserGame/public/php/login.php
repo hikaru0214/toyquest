d626464362d21838bd12e03e2a09d5a54bb4273f
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-11-20 10:12:21 +0900

    add

--- BrowserGame/public/php/login.php
+++ BrowserGame/public/php/login.php
@@ -1,41 +1,26 @@
 <?php session_start(); ?>
-<!-- DB接続 -->
-<?php require '../dbConnect/dbconnect.php'; ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログイン</title>
+<?php require '../dbConnect/dbconnect.php';?>
+<?php
 
-    <style>
-    .wrapper{
-        height:100vh;
-        width:100vw;
-        display: flex;
-        justify-content:center;
-        align-items:center;
-    }
-    </style>
+// エラーメッセージ、登録完了メッセージの初期化
+$errorMessage = "";
+$signUpMessage = "";
 
-</head>
-<body>
-
-<div style="width: 50vw; height: 50vh; margin-left: auto; margin-right: auto; text-align: center; font-size: large; position: relative;">
-
-    <form action="login-process.php" method="post">
-        メールアドレス <input id="email" name="email" type="text" style="margin-top: 50vh;">
-        <br>
-        パスワード <input id="password" name="password" type="text">
-        <br>
-        <button type="submit">ログイン</button>
-    </form>
-    <br>
-    <a href="change_password.html"><button>パスワードを忘れた方</button></a>
-    <br>
-    <a href="signup.html"><button>新規</button></a>
-</div>
-    <!-- DB切断 -->
-    <?php $pdo = null;?>
-</body>
-</html>
+$sql=$pdo->prepare('select * from User where mailaddress = ? AND password = ?');
+$sql->execute([$_POST['email'],$_POST['password']]);
+foreach($sql as $row) {
+    $_SESSION['user']=[
+        'user_id'=>$row['user_id'],
+        'user_name'=>$row['user_name'],
+        'mailaddress'=>$_POST['mailaddress'],
+        'password'=>$row['password']
+    ];
+}
+if(isset($_SESSION['user'])){
+header('Location: top.php');
+exit;
+}else{
+header('Location: login.php?hogeA=ログイン名またはパスワードが違います');
+exit;
+}
+?>
