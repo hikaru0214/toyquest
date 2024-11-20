0f59b0add9ee2e3690288d88ab891f17b802a41d
2201150 <147681633+2201150@users.noreply.github.com>
2024-11-20 10:15:04 +0900

    top

--- BrowserGame/public/php/top2.php
+++ BrowserGame/public/php/top2.php
@@ -161,6 +161,15 @@
     let isMovingRight = false;
     let isMovingUp = false;
     let isMovingDown = false;
+    let isKey_q=false;
+    let isKey_w=false;
+    let isKey_e=false;
+    let isKey_x=false;
+    let isKey_a=false;
+    let isKey_s=false;
+    let isKey_d=false;
+    let isKey_u=false;
+    let isKey_i=false;
     
 
     // キーが押された際にフラグを更新。Arrowは矢印(移動)
@@ -186,6 +195,33 @@
             isMovingDown = true;
             event.preventDefault();
         }
+        if (event.code === "KeyX") {
+            isKey_x = true;
+        }
+
+        if (event.code === "KeyQ") {
+            isKey_q = true;
+        }if (event.code === "KeyW") {
+            isKey_w = true;
+        }if (event.code === "KeyE") {
+            isKey_e = true;
+        }
+        if (event.code === "KeyA") {
+            isKey_a = true;
+        }if (event.code === "KeyS") {
+            isKey_s = true;
+        }if (event.code === "KeyD") {
+            isKey_d = true;
+        }
+        if (event.code === "KeyU") {
+            isKey_u = true;
+            acitionmix();
+        }
+        if (event.code === "KeyI") {
+            isKey_i = true;
+            acitionmix();
+        }
+
         
     });
 
@@ -211,6 +247,31 @@
             isMovingDown = false;
             event.preventDefault();
         }
+        if (event.code === "KeyX") {
+            isKey_x = false;
+        }
+
+        if (event.code === "KeyQ") {
+            isKey_q = false;
+        }if (event.code === "KeyW") {
+            isKey_w = false;
+        }if (event.code === "KeyE") {
+            isKey_e = false;
+        }
+        if (event.code === "KeyA") {
+            isKey_a = false;
+        }if (event.code === "KeyS") {
+            isKey_s = false;
+        }if (event.code === "KeyD") {
+            isKey_d = false;
+        }
+        if (event.code === "KeyU") {
+            isKey_u = false;
+        }
+        if (event.code === "KeyI") {
+            isKey_i = false;
+        }
+        
     });
 
     function acitionmix(){
