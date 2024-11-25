a7d227f4ff8d7f310208dd9e49a4159de65127a6
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-11-25 11:41:27 +0900

    add

--- BrowserGame/public/php/logout-complete.php
+++ BrowserGame/public/php/logout-complete.php
@@ -1,13 +1,17 @@
 <?php
 session_start();
-?>
+if (isset($_SESSION['User'])) {
+    unset($_SESSION['User']);
+}
 
+session_destroy(); 
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログアウト</title>
+    <title>ログアウト完了</title>
     <style>
         body {
             display: flex;
@@ -24,14 +28,14 @@ session_start();
     </style>
 </head>
 <body>
-
+<?php unset($_SESSION['User']);?>
 <div class="container">
-    <p style="font-size: 28px;">現在のアカウントからログアウトしますか？</p>
+    <p style="font-size: 28px;">ログアウトが完了しました</p>
     <div class="buttons">
-        <input type="button"  onclick="history.back()" value="いいえ" style="width:100px; height:40px; margin-right: 50px; font-size:20px;">
-        <input type="button"  onclick="location.href='./logout_complete.php'" value="はい"style="width:100px; height:40px; font-size:20px;">
+        <input type="button"  onclick="location.href='./login.php'" value="ログイン画面へ"style="width:100px; height:40px; font-size:12px;">
     </div>
 </div>
 
 </body>
 </html>
+    
