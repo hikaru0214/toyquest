3ee3c4b7f9152b36dd6a3e833f08e49a7b848bf2
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-11-27 09:43:17 +0900

    add

--- BrowserGame/public/php/signup-complete.php
+++ BrowserGame/public/php/signup-complete.php
@@ -2,12 +2,12 @@
 session_start();
 
 // トークン確認
-if (!isset($_SESSION['access_token'])) {
+/*if (!isset($_SESSION['access_token'])) {
     echo '<h1>エラー</h1>';
     echo '<p>このページは直接アクセスできません。</p>';
     echo '<a href="login.php">ログインページへ</a>';
     exit;
-}
+}*/
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -15,10 +15,40 @@ if (!isset($_SESSION['access_token'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>登録完了</title>
+    <style>
+        body {
+            background-image:url(../img/wanted_top.jpg);
+            background-size:cover;
+            background-attachment: fixed;
+            background-position: center center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+        }
+        .container {
+            text-align: center;
+            width: 46%;
+            display: flex;
+            flex-direction: column; /* 縦方向に配置 */
+            align-items: center;    /* 中央揃え */
+            margin-top: 150px;      /* 全体の位置調整 */
+        }
+        h1 {
+            font-size: 30px;
+        }
+        .links {
+            margin-top: 30px; /* 適用されるよう修正 */
+            font-size: 24px;
+        }
+    </style>
 </head>
 <body>
-    <h1>登録完了</h1>
-    <p>ユーザー登録が完了しました。</p>
-    <a href="login.php">ログインページへ</a>
+    <div class="container">
+        <h1>ユーザー登録が完了しました。</hi>
+    <div class="links">
+        <a href="login.php">ログインページへ</a>
+    </div>
+</div>
 </body>
 </html>
