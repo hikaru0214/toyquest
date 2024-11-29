d273264d0d8ede6a6d4ebadc1a1a3ce116dc956d
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-11-29 10:04:09 +0900

    add

--- BrowserGame/public/php/signup-check.php
+++ BrowserGame/public/php/signup-check.php
@@ -20,25 +20,92 @@ if (!isset($_SESSION['username'], $_SESSION['mailaddress'], $_SESSION['password'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/signup-check.css">
     <title>登録確認画面</title>
+    <style>
+            body {
+            background-image:url(../img/wanted_top.jpg);
+            background-size:cover;/*全画面*/
+            background-attachment: fixed;         /* 固定 */
+            background-position: center center;   /* 縦横中央 */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            /*min-height: 100vh;*/
+            margin: 0;
+        }
+        .container {
+            text-align: center;
+            width: 46%;
+        }
+        h1 {
+            position: relative;
+            font-size: 40px;
+            top:40px;
+            margin-bottom: 40px;
+        }
+        .form-group {
+            margin-top: 30px;
+            text-align: left;
+            position: relative;
+        }
+        .form-group label {
+            font-size: 20px;
+            display: inline-block;
+            /*margin-right: 10px;*/
+        }
+        .form-group input {
+            width: 100%;
+            height: 30px;
+            font-size: 18px;
+            padding: 15px;
+            box-sizing: border-box;
+        }
+        .login-btn {
+            display: block;
+            width: 100%;
+            height: 40px;
+            margin-top: 50px;
+            font-size: 16px;
+            background-color: #000;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+        }
+        .links {
+            margin-top: 18px;
+            font-size: 18px;
+            text-align: right;
+        }
+        .links a {
+            text-decoration: none;
+            color: #000;
+        }
+        .links a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
-    <div style="text-align: center">
+    <div class="container">
+        <h1>登録確認</h1>
         <form action="signup-ok.php" method="post">
-            <h1>登録確認</h1>
-            <hr>
-            <dl>
-                <dt>ユーザーネーム</dt>
-                <dd><input type="text" name="username" value="<?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8'); ?>"></dd>
-                <dt>メールアドレス</dt>
-                <dd><input type="mailaddress" name="mailaddress" value="<?php echo htmlspecialchars($_SESSION['mailaddress'], ENT_QUOTES, 'UTF-8'); ?>"></dd>
-                <dt>パスワード</dt>
-                <dd><input type="password" name="password" value="<?php echo str_repeat('*', strlen($_SESSION['password'])); // 表示をマスク ?>"></dd>
-            </dl>
-            <p><button type="submit">登録</button></p>
+            <div class="form-group">
+                <label for="username">ユーザーネーム</label>
+                <input type="text" name="username" value="<?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8'); ?>">
+            </div>
+            <div class="form-group">
+                <label for="mailaddress">メールアドレス</label>
+                <input type="mailaddress" name="mailaddress" value="<?php echo htmlspecialchars($_SESSION['mailaddress'], ENT_QUOTES, 'UTF-8'); ?>">
+            </div>
+            <div class="form-group">
+                <label for="password">パスワード</label>
+                <input type="password" name="password" value="<?php echo str_repeat('*', strlen($_SESSION['password']));  ?>">
+            </div>
+            <button type="submit">登録</button>
         </form>
-        <a href="signup.php">戻る</a>
+            <div class="links">
+                <a href="signup.php">戻る</a>
+            </div>
     </div>
 </body>
 </html>
