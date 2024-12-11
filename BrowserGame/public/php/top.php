aa89d46bc8609d8102f241d738cba71b5d201067
Ryusei081 <2201125@s.asojuku.ac.jp>
2024-12-11 10:42:58 +0900

    top

--- BrowserGame/public/php/top.php
+++ BrowserGame/public/php/top.php
@@ -1,4 +1,13 @@
-<?php session_start();?>
+<?php session_start();
+    if (!isset($_SESSION['user'])) {
+        // セッションがない場合の処理
+        header('Location: login.php');
+        exit;
+    }
+    
+    // セッションがある場合の処理
+    echo "ようこそ、" . htmlspecialchars($_SESSION['user']['user_name'], ENT_QUOTES, 'UTF-8') . " さん！";
+?>
 <?php require '../dbConnect/dbconnect.php';?>
 <!DOCTYPE html>
 <html>
