ebd0c7473493c9427775a8d83a5210018579d1aa
2201150 <147681633+2201150@users.noreply.github.com>
2024-12-12 11:29:59 +0900

    top

--- BrowserGame/public/php/top.php
+++ BrowserGame/public/php/top.php
@@ -328,7 +328,8 @@
 </head>
 <body>
     <div class="container">
-    <h3 class="player"><?php echo $_SESSION['user']['user_name'];?></h3>
+    <!--<h3 class="player"><?php echo $_SESSION['user']['user_name'];?></h3>-->
+    <h3 class="player">プレイヤー名</h3>
     <input type="image" src="../img/pencil_icon.png" class="pencil" onclick="">
 
     <input type="button" class="button" onclick="location.href='rogocontrol.php'" value="チャリ走"></button>
